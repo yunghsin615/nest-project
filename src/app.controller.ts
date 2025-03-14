ae544870b692161e3297b3d0c3578b2f1b6b9a27
Kaydence <duo880615@gmail.com>
2025-03-14 14:49:34 +0800

    [1-1] add function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('getCurrentTime')
+  getCurrentTime(): string {
+    return this.appService.getCurrentTime();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getCurrentTime(): string {
+    return new Date().toLocaleTimeString();
+  }
 }
